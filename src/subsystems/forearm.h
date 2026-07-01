f017315c34932f4bf9be7d65de0bd3236ae146a4
Jackson Scott <bubbarooski08@gmail.com>
2026-07-01 09:40:30 -0500

    First Draft of Forearm header file

---
+++ src/subsystems/forearm.h
@@ -0,0 +1,84 @@
+#ifndef __FOREARM_H__ 
+#define __FOREARM_H__ 
+
+#include "../architecture/subsystem.h"   
+#include "../control/feedForward.h" 
+#include "../control/pidcontroller.h"   
+#include "../control/trapezoidalMotion.h"
+
+typedef enum { 
+   PURSUING,  
+   HOLDING
+} ForearmState;
+
+class Forearm : public Subsystem {  
+    private:
+
+       static double PLACE_SETPOINT; 
+       static double PRIMING_SETPOINT; 
+       static double GROUND_SETPOINT; 
+       static double STANDING_SETPOINT;  
+
+       static double ANGULAR_ERROR_TOLERANCE; 
+
+       static Forearm* globalPtr = nullptr;     
+
+       double currentAngle; 
+       double currentVelocity; 
+
+       vex::motor forearmMotor;  
+       vex::rotation rotarySensor;    
+
+       AngularArmFFConstants armFFConsts; //Bulk (feedforward) 
+
+       pidcontroller* feedback = nullptr; //Rest done with feedback
+       PIDConstants pidConsts; 
+
+       TrapezoidConstants motionConsts; 
+       TrapezoidalMotionProfile* motionProfile = nullptr;
+        
+       double angularDeadZones[2];  
+       
+       double calculateOutput(double omega, double alpha); //velocity and acceleration but for angles
+
+       double getCurrentAngle();   
+       double getVelocity();  
+
+       void setSetpoint(double setpoint); 
+
+       double angularSetpoint; 
+
+       ForearmState currentState = ForearmState::HOLDING; 
+
+       void stateControl();  //ONLY (We can't manually modify the forearm with the controller)
+
+    public:   
+       using Subsystem::get;  
+
+       static Forearm& getObject();  
+
+       Forearm(): 
+         Subsystem( 
+            "forearm", 
+            {  
+               (EntrySet){"task_id", EntryType::INT}, 
+               (EntrySet){"active", EntryType::BOOL}, 
+               (EntrySet){"at_setpoint", EntryType::BOOL}
+            }
+         ), 
+         forearmMotor(vex::motor(vex::PORT16)), 
+         rotarySensor(vex::rotation(vex::PORT12)) 
+         { 
+            globalPtr = this;
+         };   
+
+    protected: 
+         using Subsystem:set; 
+    
+      
+}; 
+
+
+  
+
+#endif
