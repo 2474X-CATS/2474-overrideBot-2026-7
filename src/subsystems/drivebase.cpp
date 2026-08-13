382c72dcb73e926c38140f0677455ab6a2c5ac26
Jackson Scott <bubbarooski08@gmail.com>
2026-08-12 22:39:12 -0500

    Added very basic drive functions
    - Arcade drive
    - Driving (auto)
    - Turning (auto)

---
+++ src/subsystems/drivebase.cpp
@@ -0,0 +1,49 @@
+#include "drivebase.h" 
+
+Drivebase* Drivebase::globalPtr = nullptr; 
+
+void Drivebase::init(){  
+  ffConsts.kA = 0; 
+  ffConsts.kV = 0; 
+  ffConsts.kS = 0; 
+   
+  correctionConsts.P = 0; 
+  correctionConsts.I = 0; 
+  correctionConsts.D = 0; 
+
+
+  leftMotors.setStopping(vex::brakeType::brake);  
+  rightMotors.setStopping(vex::brakeType::coast);
+}  
+
+Drivebase& Drivebase::getObject(){ 
+    return *globalPtr;
+}
+
+void Drivebase::periodic(){ 
+  arcadeDrive(RobotState::getAxisState(AxisType::M_LEFT_VERTICAL), RobotState::getAxisState(AxisType::M_RIGHT_HORIZONTAL));
+}
+
+void Drivebase::updateTelemetry(){ 
+  return;
+}
+
+void Drivebase::stop(){ 
+   leftMotors.stop(); 
+   rightMotors.stop();
+} 
+
+void Drivebase::manualDrive(double voltage){ 
+    leftMotors.spin(vex::directionType::fwd, voltage, vex::voltageUnits::volt); 
+    rightMotors.spin(vex::directionType::rev, voltage, vex::voltageUnits::volt);
+} 
+
+void Drivebase::manualSpin(double voltage){ 
+    leftMotors.spin(vex::directionType::fwd, -voltage, vex::voltageUnits::volt); 
+    rightMotors.spin(vex::directionType::rev, voltage, vex::voltageUnits::volt); 
+} 
+
+void Drivebase::arcadeDrive(double speed, double rotation){ 
+    leftMotors.spin(vex::directionType::fwd, ((speed + rotation) / 100.0) * 12.0, vex::voltageUnits::volt); 
+    rightMotors.spin(vex::directionType::rev, ((speed + rotation) / 100.0) * 12.0, vex::voltageUnits::volt); 
+}
--- src/subsystems/drivebase.h
+++ src/subsystems/drivebase.h
@@ -3,7 +3,8 @@
 
 
 #include "../architecture/subsystem.h"  
-#include "../control/feedForward.h"
+#include "../control/feedForward.h" 
+#include "../control/pidcontroller.h"
 
 class Drivebase : public Subsystem { 
     public:  
@@ -17,13 +18,22 @@ class Drivebase : public Subsystem {
         { 
           (EntrySet){"is_on", EntryType::BOOL}
         }
-      ){ 
+      ), 
+      leftFront(vex::motor(vex::PORT4)), 
+      leftBack(vex::motor(vex::PORT5)), 
+      rightFront(vex::motor(vex::PORT6)), 
+      rightBack(vex::motor(vex::PORT7)), 
+      leftMotors(leftFront, leftBack), 
+      rightMotors(rightFront, rightBack)
+      { 
         globalPtr = this;
       };
       
-      void manualDrive(double speed, double acceleration); 
+      void manualDrive(double voltage); 
       
-      void manualSpin(double voltage); //Negative for the opposite direction
+      void manualSpin(double voltage); //Negative for the opposite direction 
+
+
 
     private:     
       static Drivebase* globalPtr;
@@ -32,11 +42,16 @@ class Drivebase : public Subsystem {
       vex::motor leftBack; 
 
       vex::motor rightFront; 
-      vex::motor rightBack;    
+      vex::motor rightBack;     
 
-      FFConstants ffConsts;   
+      vex::motor_group leftMotors;
+      vex::motor_group rightMotors;
 
+      FFConstants ffConsts;
+      PIDConstants correctionConsts;
+      
       void arcadeDrive(double speed, double rotation); 
+      
 
     protected: 
       using Subsystem::set; 
@@ -46,7 +61,7 @@ class Drivebase : public Subsystem {
       void updateTelemetry() override; 
       void stop() override; 
 
-}
+};
 
 
 #endif
