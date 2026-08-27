1a22a7ad0b13d690ce4cb2d0a8b17b3be0f1e944
Jackson Scott <bubbarooski08@gmail.com>
2026-08-26 21:28:54 -0500

    Syncing control changes that occured on upstream branches

--- src/control/feedForward.cpp
+++ src/control/feedForward.cpp
@@ -1,12 +1,22 @@
 #include "feedForward.h"
 #include "math.h"
 
-double FeedForward::calculate(double velocity, double acceleration)
-{
-    return kS * copysign(1, velocity) + kV * velocity + kA * acceleration;
-};
 
-double FeedForward::calculate(double velocity)
-{
-    return calculate(velocity, 0);
-};
+double FFConstants::calculate(double velocity, double acceleration){ //m/s
+    return kS * copysign(1, velocity) + kV * velocity + kA * acceleration;
+} 
+
+double AngularArmFFConstants::calculate(double theta, double velocity, double acceleration){ // rot/s   
+    double static_output;  
+    if (velocity == 0){ 
+        static_output = 0; 
+    } else { 
+        static_output = (copysign(1, velocity)) * kS_rot;
+    }
+      
+    return ((kCos * kCos_ratio) * cos(theta)) + static_output + (kV_rot * velocity) + (kA_rot * acceleration);
+}
+
+double ElevatorFFConstants::calculate(double velocity, double acceleration){ // m/s
+    return kG + ffConsts.calculate(velocity, acceleration);
+}
